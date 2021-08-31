d716069ea972549528a4d1aae4bc914246af9542
AndreyBaglai <andreybaglai@gmail.com>
2021-08-31 16:01:19 +0300

    feat: add form ant component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
 import React, { useState } from 'react';
+
+import { PageHeader, Button, Row, Col, Input, List, Avatar, Card, Form } from 'antd';
+import { CloseOutlined } from '@ant-design/icons';
+
 import 'antd/dist/antd.css';
 
-import { PageHeader, Button, Row, Col, Input, List, Avatar, Card } from 'antd';
+const { Search, TextArea } = Input;
 
-const { Search } = Input;
+const validateMessages = {
+  required: '${label} is required!',
+  types: {
+    email: '${label} is not a valid email!',
+    number: '${label} is not a valid number!',
+  },
+  number: {
+    range: '${label} must be between ${min} and ${max}',
+  },
+};
 
 function App() {
   const [paths, setPaths] = useState([
     { title: '1', body: 'Some text 1' },
     { title: '2', body: 'Some text 2' },
   ]);
+
+  const onCreatePath = (data: any) => console.log(data);
 
   return (
     <>
@@ -54,6 +69,42 @@ function App() {
           </Col>
         </Row>
       </main>
+      <div>
+        <PageHeader
+          title="Add new path"
+          extra={[<Button key="2" type="primary" shape="circle" icon={<CloseOutlined />} />]}
+        />
+        <Row>
+          <Col span={12}>
+            <Form name="new-path" onFinish={onCreatePath} validateMessages={validateMessages}>
+              <Form.Item
+                name="title"
+                label="Title"
+                rules={[
+                  {
+                    required: true,
+                  },
+                ]}>
+                <Input />
+              </Form.Item>
+
+              <Form.Item name="short-description" label="Short description">
+                <TextArea />
+              </Form.Item>
+              <Form.Item name="full-description" label="Full description">
+                <TextArea />
+              </Form.Item>
+              <div>Length: 1.13 km</div>
+              <Form.Item>
+                <Button type="primary" htmlType="submit">
+                  Add path
+                </Button>
+              </Form.Item>
+            </Form>
+          </Col>
+          <Col span={12}>Map form</Col>
+        </Row>
+      </div>
     </>
   );
 }
