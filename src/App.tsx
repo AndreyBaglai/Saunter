fa4397dac223d8e2caa46e1ff69c6afa53d21cfe
AndreyBaglai <andreybaglai@gmail.com>
2021-08-31 11:28:17 +0300

    feat: add enviroment file .env in root directory

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import React from 'react';
 function App() {
   return (
     <div>
-      Hello world!!!
+      <p>NODE_ENV: {process.env.NODE_ENV}</p>
+      <h2>REACT_APP: {process.env.REACT_APP_API_KEY}</h2>
     </div>
   );
 }
