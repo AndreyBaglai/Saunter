5cd1fa69076f4644f7278d2ca746a2a97669ba6e
AndreyBaglai <andreybaglai@gmail.com>
2021-08-31 12:41:30 +0300

    feat: add antd package create PageHeader component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import 'antd/dist/antd.css';
+
+import { PageHeader, Button } from 'antd';
 
 function App() {
   return (
     <div>
-      <p>NODE_ENV: {process.env.NODE_ENV}</p>
-      <h2>REACT_APP: {process.env.REACT_APP_API_KEY}</h2>
+      <PageHeader title="Saunter" extra={[<Button key="1" type="primary">Add path</Button>]}/>
     </div>
   );
 }
