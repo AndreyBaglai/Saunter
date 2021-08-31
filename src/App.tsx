27a9d636d36cb3288d7b8ff16eb3193f142489ad
AndreyBaglai <andreybaglai@gmail.com>
2021-08-31 14:05:06 +0300

    feat: add search and list paths antd components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'antd/dist/antd.css';
 
-import { PageHeader, Button } from 'antd';
+import { PageHeader, Button, Row, Col, Input, List, Avatar } from 'antd';
+
+const { Search } = Input;
 
 function App() {
+  const [paths, setPaths] = useState([
+    { title: '1', body: 'Some text 1' },
+    { title: '2', body: 'Some text 2' },
+  ]);
+
   return (
-    <div>
-      <PageHeader title="Saunter" extra={[<Button key="1" type="primary">Add path</Button>]}/>
-    </div>
+    <>
+      <PageHeader
+        title="Saunter"
+        extra={[
+          <Button key="1" type="primary">
+            Add path
+          </Button>,
+        ]}
+      />
+      <main>
+        <Row>
+          <Col span={12}>
+            <Search placeholder="Input search text" onSearch={() => {}} enterButton />
+            <List
+              dataSource={paths}
+              renderItem={(PaymentMethodChangeEvent: any) => (
+                <List.Item key={PaymentMethodChangeEvent.body + PaymentMethodChangeEvent.path}>
+                  <List.Item.Meta
+                    avatar={
+                      <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+                    }
+                    title={<a href="https://ant.design">{PaymentMethodChangeEvent.body}</a>}
+                    description={PaymentMethodChangeEvent.body}
+                  />
+                  <div>Content</div>
+                </List.Item>
+              )}></List>
+          </Col>
+          <Col span={12}>col-12 Right side</Col>
+        </Row>
+      </main>
+    </>
   );
 }
 
